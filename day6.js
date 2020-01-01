6069c2fef3735083b9ec21387b75dcd6b124e66e
Petar Dzhambazov <jamby77@gmail.com>
2020-01-01 13:43:15 +0200

    day 6 complete

--- day6.js
+++ day6.js
@@ -18,19 +18,61 @@ const centers = input.map(item => {
 const leafs = input.map(item => item.tail);
 
 const root = centers.find(center => leafs.indexOf(center) === -1);
-function buildTree(tree, pairs, root) {
+let total = 0;
+function buildTree(tree, pairs, root, level = 0) {
   // console.log(tree, pairs, root);
   const descendants = pairs.filter(c => c.head === root);
   const rest = pairs.filter(c => c.head !== root);
-  if (descendants.length > 0) {
-    tree[root] = {};
-  }
+  tree[root] = { level, name: root, descendants: [] };
+  total += level;
 
   descendants.forEach(item => {
     const { tail } = item;
-    buildTree(tree[root], rest, tail);
+    tree[root].descendants.push(tail);
+    buildTree(tree[root], rest, tail, level + 1);
   });
 }
 
+function findNode(tree = {}, search = "") {
+  const { level, name, descendants, ...rest } = tree;
+  if (name === search) {
+    return { ...tree, ancestors: [] };
+  }
+  if (!rest) {
+    return null;
+  }
+
+  for (let i = 0; i < descendants.length; i++) {
+    const descName = descendants[i];
+    const found = findNode(rest[descName], search);
+    if (found) {
+      found.ancestors.unshift(name);
+      return found;
+    }
+  }
+
+  return null;
+}
+
 buildTree(tree, input, root);
-console.log(tree);
+// part 1 answer
+console.log(total);
+
+const you = findNode(tree["COM"], "YOU");
+const san = findNode(tree["COM"], "SAN");
+if (you && san) {
+  let commonName;
+  const c =
+    you.ancestors.length > san.ancestors.length
+      ? san.ancestors.length
+      : you.ancestors.length;
+  for (let i = 0; i < c; i++) {
+    if (you.ancestors[i + 1] !== san.ancestors[i + 1]) {
+      commonName = you.ancestors[i];
+      break;
+    }
+  }
+  const common = findNode(tree["COM"], commonName);
+  // part 2 answer
+  console.log(you.level - 1 - common.level + san.level - 1 - common.level);
+}

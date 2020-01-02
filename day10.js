89a425eee629692b510b5a299d3e5cb0c835541f
Petar Dzhambazov <pdzhambazov@pros.com>
2020-01-02 13:45:22 +0200

    day 10 wip

--- day10.js
+++ day10.js
@@ -20,12 +20,14 @@ function getSlope(a, b) {
 }
 
 const slopes = {};
-
+const targets = {};
+const makeHash = ast => `y${ast.y}x${ast.x}`;
 for (let i = 0; i < field.length; i++) {
   const a = field[i];
-  const hash = `y${a.y}x${a.x}`;
+  const hash = makeHash(a);
 
   slopes[hash] = new Set();
+  targets[hash] = [];
   for (let j = 0; j < field.length; j++) {
     const b = field[j];
     if (a === b) {
@@ -33,14 +35,74 @@ for (let i = 0; i < field.length; i++) {
     }
     const slope = getSlope(a, b);
     slopes[hash].add(slope);
+    targets[hash].push({ target: makeHash(b), slope });
   }
 }
+
 let mostVisible = 0;
+let laserStattion;
 Object.keys(slopes).forEach(hash => {
   const hashSet = slopes[hash];
   if (hashSet.size > mostVisible) {
     mostVisible = hashSet.size;
+    laserStattion = hash;
   }
 });
+
 // part 1, solved with help from https://www.reddit.com/r/adventofcode/comments/e8mj2w/2019_day_10_part_1_having_a_hard_time_understand/
 console.log(mostVisible);
+
+// part 2
+laserStattion;
+let laserTargets = targets[laserStattion];
+
+const parseHash = hash => {
+  const result = /y(\d+)x(\d+)/g.exec(hash);
+  const [_, x, y] = result;
+  return { x, y };
+};
+
+function distance(a, b) {
+  const result = Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+  return result;
+}
+
+const hashDistance = (a, b) => {
+  const astA = parseHash(a);
+  const astB = parseHash(b);
+  return distance(astA, astB);
+};
+
+laserTargets.sort((a, b) => {
+  const aSlope = a.slope,
+    bSlope = b.slope;
+  if (aSlope > 0 && bSlope < 0) {
+    return -1;
+  }
+  if (bSlope > 0 && aSlope < 0) {
+    return 1;
+  }
+
+  if (aSlope === bSlope) {
+    const ad = hashDistance(laserStattion, a.target);
+    const bd = hashDistance(laserStattion, b.target);
+    return ad > bd ? 1 : -1;
+  }
+  return aSlope - bSlope;
+});
+
+let targetNum = 0;
+let lastSlope;
+for (let i = 0; i < laserTargets.length; i++) {
+    const target = laserTargets[i];
+    if (lastSlope !== target.slope) {
+        console.log(i, targetNum , target);
+        lastSlope = target.slope;
+        targetNum++;
+        if (targetNum === 200) {
+            console.log(target.target);
+            break;
+        }
+    }
+}
+console.log(laserTargets);
